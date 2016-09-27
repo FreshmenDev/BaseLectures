bc635668f44384c66df802ffbd84449e1f912edf
Pavel Orlov <garffans@outlook.com>
2016-09-27 11:02:20 +0300

    Алгоритм поиска в массиве

---
+++ lection14.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main () {
+    int n = 5;
+    int ages[n];
+    int value = 10;
+
+    for (int i = 0; i < n; ++i) {
+        ages[i] = i + 10;
+    }
+
+    for (int i = 0; i < n; ++i) {
+        std::cout << ages[i] << " " << std::endl;
+    }
+
+    int j = 0, ind = -1;
+
+    while (j < n) {
+        if (ages[j] == value) {
+            ind = j;
+            break;
+        }
+        ++j;
+    }
+    std::cout << ind << " ";
+
+    return 0;
+}
