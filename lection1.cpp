7749fdb7459cd0aaba6e5f1a496daa9abbd4f4a1
Pavel Orlov <garffans@outlook.com>
2016-09-27 10:48:06 +0300

    Добавил пример работы с обычными переменными

---
+++ lection1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main () {
+    int count = 5436;
+
+    int count2 = count;
+
+    count2 = 10 + 30 * 423423;
+    std::cout << count << " " << count2 << std::endl;
+    return 0;
+}
