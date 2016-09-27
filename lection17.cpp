46b7516aab922b4e9dee484f3ef568496e84b3a5
Pavel Orlov <garffans@outlook.com>
2016-09-27 11:06:40 +0300

    Создание строки с указателем

---
+++ lection17.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main () {
+    char* string = "Hello World";
+
+    std::cout << string << std::endl;
+
+    for (int i = 0; i < 10; ++i) {
+        std::cout << string[i] << " ";
+    }
+
+    return 0;
+}
