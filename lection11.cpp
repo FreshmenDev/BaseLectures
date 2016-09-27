99a8fa49ddb6cd4510054328a0b7af07e3565fba
Pavel Orlov <garffans@outlook.com>
2016-09-27 10:53:47 +0300

    Добавил пример работы с указателями

--- lection11.cpp
+++ lection11.cpp
@@ -4,9 +4,8 @@ int main () {
     int count = 5436;
 
     int* count2 = &count;
-
     std::cout << count <<  " " << count2 << " " << *count2 << std::endl;
-
+    
     *count2 = 10 + 30 * 423423;
     std::cout << count << " " << count2 << " " << *count2 << std::endl;
     return 0;
