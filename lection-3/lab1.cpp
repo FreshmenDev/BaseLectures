b12ddca1374242bbb0f7fee0b1ea26aeef973f9c
Igor Donskoi <zazaaqaq@inbox.ru>
2016-10-02 00:45:10 +0300

    Добавил метод пузырька для сортировки 2-ч массивов

--- lection-3/lab1.cpp
+++ lection-3/lab1.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main() {
     const int NEWS_COUNT = 4;
     const double RADIUS = 10.0;
-    
+    float a[NEWS_COUNT];
     char* news[] = {
      "Fire",
      "Flood",
@@ -29,10 +29,13 @@ int main() {
      56.6,
      43.6
     };
+
+	char* b[NEWS_COUNT];
+	char* c;
     
     cout << "News in your region:" << endl << endl;
     
-    float myLat, myLon;
+    float myLat, myLon,tmp,distance;
     
     cout << "Enter your position" << endl;
     
@@ -41,11 +44,33 @@ int main() {
     bool hasNews = false;
     
     
-    for(int i = 0; i < NEWS_COUNT; ++i) {
-        double distance = sqrt(pow((myLat - lats[i]), 2) + pow((myLon - lons[i]), 2));   
-       
-        if(distance < RADIUS) {
-            cout << news[i] << endl;
+    for(int i = 0; i < NEWS_COUNT; ++i) 
+	 {  
+	   distance =sqrt(pow((myLat - lats[i]), 2) + pow((myLon - lons[i]), 2));
+       if(distance < RADIUS) 
+	    {
+		 a[i] = distance;
+		 b[i] = news[i];           
         }   
+     }
+  
+	//Метод сортировки пузырька
+	for(int i = 0; i < NEWS_COUNT- 1; ++i) // i - номер прохода
+    {            
+        for(int j = 0; j < NEWS_COUNT- 1; ++j) // внутренний цикл прохода
+        {     
+            if (arr[j + 1] < arr[j]) 
+            {
+                tmp = a[j + 1]; 
+                a[j + 1] = a[j]; 
+                a[j] = tmp;
+
+				c = b[j + 1]; 
+                b[j + 1] = b[j]; 
+                b[j] = c;
+            }
+        }
     }
+
+
 }
