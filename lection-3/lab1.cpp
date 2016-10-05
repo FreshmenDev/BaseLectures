a4434f460569d9ca8eba9a67618c54519c07f121
Igor Donskoi <zazaaqaq@inbox.ru>
2016-10-05 18:51:20 +0300

    Добавил все необходимые переменные

--- lection-3/lab1.cpp
+++ lection-3/lab1.cpp
@@ -5,4 +5,41 @@
 #include <cmath>
 #include <clocale>
 
-using namespace std; //чтобы не писать постоянно std::
+using namespace std; //Чтобы не писать постоянно std::
+
+const int NEWS_COUT = 4; //Константы
+const double RADIUS =10.0;
+
+struct News //Описание структуры
+ {
+	 char* description;
+	 double lat;
+	 double lon;
+	 double distance;
+ };
+
+int main()
+	setlocale(LC_ALL,"RUS"); //Подключение поддержки русского языка
+
+    News news [NEWS_COUT]; //Создание структуры данных
+
+	 news[1].description = "Fire"; //Записывает названия новостей в структуру
+	 news[2].description = "Flood";
+	 news[3].description = "Asteroid";
+	 news[4].description = "Citi day";
+
+	 news[1].lat = 45.5; //Записывает долготу в структуру
+	 news[2].lat = 48.6;
+	 news[3].lat = 58.6;
+	 news[4].lat = 60.6;
+
+	 news[1].lon = 40.5; //Записывает широту в структуру
+	 news[2].lon = 47.6; 
+	 news[3].lon = 56.6; 
+	 news[4].lon = 43.6; 
+
+	 char* c;
+	 double myLat,myLon,tmp,distance; //Остальные необходимые переменные 
+	 
+
+
